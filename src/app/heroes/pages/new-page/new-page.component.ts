964d02c69009c779dceeae22ba8806b0b05bb5d5
Daniel Esteban <decepeda@misena.edu.co>
2023-07-05 19:32:26 -0500

    Actualizar y crear heroes, se establece el valor del formulario

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {Hero, Publisher} from "../../interfaces/hero.interface";
 import {HeroesService} from "../../services/heroes.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-new-page',
@@ -9,7 +11,7 @@ import {HeroesService} from "../../services/heroes.service";
   styles: [
   ]
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   public heroForm = new FormGroup({
     id:               new FormControl(''),
@@ -26,7 +28,20 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
-  constructor( private heroesService: HeroesService ) {
+  constructor( private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private heroesService: HeroesService ) {
+  }
+  ngOnInit() {
+    if ( !this.router.url.includes('edit') ) return;
+
+    this.activatedRoute.params.pipe(
+      switchMap( ({ id }) => this.heroesService.getHeroById( id )),
+    ).subscribe( hero => {
+      if ( !hero) return this.router.navigateByUrl('/')
+      this.heroForm.reset( hero );
+      return ;
+    })
   }
 
   get currentHero(): Hero {
@@ -35,6 +50,17 @@ export class NewPageComponent {
   }
   onSubmit(): void {
     if ( this.heroForm.invalid ) return;
+    if ( this.currentHero.id ) {
+      this.heroesService.updateHero( this.currentHero )
+        .subscribe( hero => {
+        //   TODO: mostrar snackbar
+        });
+      return;
+    }
+    this.heroesService.addHero( this.currentHero )
+      .subscribe( hero => {
+      //   TODO: mostrar snackbar, y navegar a /heroes/edit/ hero.id
+      })
   }
 
 }
