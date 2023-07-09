a71e23f69c1685f171d721545f4631d95192cd0a
Daniel Esteban <decepeda@misena.edu.co>
2023-07-09 16:05:04 -0500

    Creamos metodo login en el servicio de autenticacion

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environments} from "../../../enviroments/environments";
 import {User} from "../interfaces/user.interface";
+import {Observable, tap} from "rxjs";
+import * as http from "http";
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
@@ -16,5 +18,12 @@ export class AuthService {
     if ( !this.user ) return undefined;
     // return {...this.user};
     return structuredClone( this.user );
+  }
+  login( email: string, password: string ): Observable<User> {
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user),
+        tap( user => localStorage.setItem('token', user.id.toString()))
+      );
   }
 }
